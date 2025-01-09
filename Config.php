665eec3492a941fb11cfea55f5bc47113884d3ed
Domenico Giambra <zorinik@gmail.com>
2025-01-10 00:15:36 +0100

    Fix

--- Config.php
+++ Config.php
@@ -4,7 +4,7 @@ use Model\Core\Module_Config;
 
 class Config extends Module_Config
 {
-	protected function assetsList()
+	protected function assetsList(): void
 	{
 		$this->addAsset('config', 'config.php', function () {
 			return '<?php
